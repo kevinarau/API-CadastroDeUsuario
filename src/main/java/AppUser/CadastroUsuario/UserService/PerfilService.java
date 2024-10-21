678ceba74a12189c422c6387a4351620749ef2c4
kevin araujo <kevin.aparecido97@gmail.com>
2024-10-21 17:06:29 -0300

    Commit 21/10/

--- src/main/java/AppUser/CadastroUsuario/UserService/PerfilService.java
+++ src/main/java/AppUser/CadastroUsuario/UserService/PerfilService.java
@@ -1,13 +1,20 @@
 package AppUser.CadastroUsuario.UserService;
 
 
-import AppUser.CadastroUsuario.Model.User;
-import AppUser.CadastroUsuario.Repository.userRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PerfilService {
+
+
+
+      public void atualizarBio(){
+
+      }
+
+      public void atualizarFoto(){
+
+    }
 
 
 }
