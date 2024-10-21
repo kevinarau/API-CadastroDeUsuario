d1d27586216f9183526dddfcf07a06fa5364432d
kevin araujo <kevin.aparecido97@gmail.com>
2024-10-21 17:04:16 -0300

    Commit 21/10/

--- src/main/java/AppUser/CadastroUsuario/UserService/UserService.java
+++ src/main/java/AppUser/CadastroUsuario/UserService/UserService.java
@@ -1,6 +1,7 @@
 package AppUser.CadastroUsuario.UserService;
 
 import AppUser.CadastroUsuario.Model.User;
+import AppUser.CadastroUsuario.Repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +11,16 @@ public class UserService {
 
 
     @Autowired
-    private AppUser.CadastroUsuario.Repository.userRepository userRepository;
+    private userRepository userRepository;
 
 
     public String cadastrarUsuario(String nome, String email, String password) {
         if (userRepository.findByEmail(email) != null) {
             return "Email já Cadastrado";
         }
-        if (password.length() < 6) {
-            return "Senha menor que 6 Caracteres";
-        }
+           if (password.length() < 6) {
+                return "Senha menor que 6 Caracteres";
+           }
 
         User user = new User();
         user.setNome(nome);
@@ -29,36 +30,41 @@ public class UserService {
         return "Usuario Cdastrado";
     }
 
+
+
     public String validarLogin ( String email, String password ) {
 
         User user = userRepository.findByEmail(email);
-        if (user == null){
-            return  "Usuario nao encontrado!";
-        }
+               if (user == null){
+                     return  "Usuario nao encontrado!";
+              }
 
-        if (!user.getPassword().equals(password)){
-            return "Senha incorreta!";
+                     if (!user.getPassword().equals(password)){
+                  return "Senha incorreta!";
+                     }
 
-        }
-        return " Login realizado com sucesso!";
+                           return " Login realizado com sucesso!";
     }
+
+
+
+
 
     public String atualizarPerfil(Long id, String novoNome,String NovoPassword) {
         User user = userRepository.findById(id).orElse(null);
-        if (user == null){
+
+            if (user == null){
             return  "Usuario não encontrado!";
+                          }
 
-        }
+        if (novoNome != null && novoNome.length() > 20) {
+                  user.setNome(novoNome);
+              }
 
-        if (novoNome != null && novoNome.length() > 20){
-            user.setNome(novoNome);
-        }
+                  if (NovoPassword != null && NovoPassword.length() >= 6) {
+                  user.setPassword(NovoPassword);
 
-        if (NovoPassword != null && NovoPassword.length() >= 6) {
-            user.setPassword(NovoPassword);
-
-
-        }else if (NovoPassword.length() < 6) {
+                  }else if (NovoPassword.length() < 6) {
             return   "A nova senha precisa ter pelo menos 6 caracteres.!";
         }
 
