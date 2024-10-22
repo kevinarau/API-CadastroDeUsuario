850cc540e7e5873876affc7d8cc0c68a1f45e2db
kevin araujo <kevin.aparecido97@gmail.com>
2024-10-22 17:18:51 -0300

    Commit 22/10/

--- src/main/java/AppUser/CadastroUsuario/Contollers/userControlle.java
+++ src/main/java/AppUser/CadastroUsuario/Contollers/userControlle.java
@@ -1,5 +1,6 @@
 package AppUser.CadastroUsuario.Contollers;
 import AppUser.CadastroUsuario.UserService.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/AppUser/CadastroUsuario/UserService/PerfilService.java
+++ src/main/java/AppUser/CadastroUsuario/UserService/PerfilService.java
@@ -1,12 +1,13 @@
 package AppUser.CadastroUsuario.UserService;
 
 
+import AppUser.CadastroUsuario.Repository.perfilRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PerfilService {
 
-
+   private perfilRepository perfilRepository;
 
       public void atualizarBio(){
 
